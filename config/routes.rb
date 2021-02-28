5a8f76a525fd17ac84ddc3082e95531362baaef2
vaishucn <vaishucnu@gmail.com>
2021-02-28 20:12:16 +0530

    updated database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "todos",to:"todos#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "todos",to: "todos#index"
 end
