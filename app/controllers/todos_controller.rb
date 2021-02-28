f7350cb7292802852e08ddba3547b761cabf0a2b
vaishucn <vaishucnu@gmail.com>
2021-02-28 20:53:32 +0530

    Display all todo list

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index 
-        render plain: Todo.all.to_a
+        render plain: Todo.all.map {|todo| todo.to_pleasant_string}.join("\n")
     end
 end
